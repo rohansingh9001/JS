e082c3bc9cd6e423a792c3fcf5d0cd8aa45f5fd8
rohansingh9001 <rohansingh9001@gmail.com>
2023-12-17 22:46:51 +0530

    Completed week 0

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,154 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+    this.allowedChars = '1234567890.+-*/()'
+    this.operators = ['+', '-', '*', '/'];
+    this.currentPosition = 0;
+  }
+
+  add(num) {
+    this.result += num;
+  }
+
+  subtract(num) {
+    this.result -= num;
+  }
+
+  multiply(num) {
+    this.result *= num;
+  }
+
+  divide(num) {
+    if (num == 0) {
+      throw new Error('Division by zero is not allowed');
+    }
+    this.result /= num;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  isExpressionValid(expression) {
+    var isValid = true;
+
+    for (var char of expression) {
+      if (!this.allowedChars.includes(char)) {
+        isValid = false
+      }
+    }
+
+    return isValid;
+  }
+
+  cleanExpression(expression) {
+    return expression.replace(/\s/g, '').trim();
+  }
+
+  calculate(expression) {
+    // Removes whitespace
+    expression = this.cleanExpression(expression);
+
+    if (!this.isExpressionValid(expression)) {
+      throw new Error('Unexpected character when parsing expression');
+    }
+
+    const expressionTree = this.parseExpression(expression);
+
+    this.result = this.solve(expressionTree);
+
+    return this.result;
+  }
+
+  parseExpression(expression) {
+    // Parse the expression into a tree
+    this.currentPosition = 0;
+    const result = this.parseAdditionSubtraction(expression);
+    if (this.currentPosition !== expression.length) {
+      throw new Error('Unexpected character at the end of expression');
+    }
+    return result;
+  }
+
+  parseAdditionSubtraction(expression) {
+    let left = this.parseMultiplicationDivision(expression);
+    while (this.currentPosition < expression.length) {
+      const operator = expression[this.currentPosition];
+      if (operator === '+' || operator === '-') {
+        this.currentPosition++;
+        const right = this.parseMultiplicationDivision(expression);
+        left = { type: operator, left, right };
+      } else {
+        break;
+      }
+    }
+    return left;
+  }
+
+  parseMultiplicationDivision(expression) {
+    let left = this.parsePrimary(expression);
+    while (this.currentPosition < expression.length) {
+      const operator = expression[this.currentPosition];
+      if (operator === '*' || operator === '/') {
+        this.currentPosition++;
+        const right = this.parsePrimary(expression);
+        left = { type: operator, left, right };
+      } else {
+        break;
+      }
+    }
+    return left;
+  }
+
+  parsePrimary(expression) {
+    const char = expression[this.currentPosition];
+    if (char === '(') {
+      this.currentPosition++;
+      const result = this.parseAdditionSubtraction(expression);
+      if (expression[this.currentPosition] !== ')') {
+        throw new Error('Expected closing parenthesis');
+      }
+      this.currentPosition++;
+      return result;
+    } else if (/\d/.test(char) || char === '.') {
+      let num = '';
+      while (this.currentPosition < expression.length && (/\d/.test(expression[this.currentPosition]) || expression[this.currentPosition] === '.')) {
+        num += expression[this.currentPosition];
+        this.currentPosition++;
+      }
+      return { type: 'number', value: parseFloat(num) };
+    } else {
+      throw new Error('Unexpected character in expression');
+    }
+  }
+
+  solve(root) {
+    if (root.type == 'number') {
+      return root.value;
+    }
+
+    if (root.type == '+') {
+      return this.solve(root.left) + this.solve(root.right);
+    } else if (root.type == '-') {
+      return this.solve(root.left) - this.solve(root.right);
+    } else if (root.type == '*') {
+      return this.solve(root.left) * this.solve(root.right);
+    } else {
+      var left = this.solve(root.left);
+      var right = this.solve(root.right);
+      if (right == 0) {
+        throw new Error('Division by zero is not allowed');
+      }
+      return left / right;
+    }
+  }
+}
 
 module.exports = Calculator;
--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,43 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
 
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(index) {
+    if ((index < 0) | (index >= this.todos.length)) {
+      return
+    }
+
+    this.todos.splice(index, 1);
+  }
+
+  update(index, todo) {
+    if ((index < 0) | (index >= this.todos.length)) {
+      return
+    }
+    this.todos[index] = todo;
+  }
+
+  get(index) {
+    if ((index < 0) | (index >= this.todos.length)) {
+      return null;
+    }
+    return this.todos[index];
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  clear() {
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
